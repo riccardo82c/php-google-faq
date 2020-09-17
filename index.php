f9893b3621c50858ed5903b64720114ee1c69b72
Riccardo Colombo <rikyregna@gmail.com>
2020-09-17 15:25:51 +0200

    init

---
+++ index.php
@@ -0,0 +1,18 @@
+<!-- /* Riscrivere questa pagina del sito google https: //policies.google.com/faq.
+Ci sono diverse domande con relative risposte. Gestire il “Database” e la visualizzazione di queste domande e risposte con PHP. */ -->
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="css/style.css">
+	<title>Faq Google</title>
+</head>
+<body>
+
+<h1>prova</h1>
+	
+</body>
+</html>
