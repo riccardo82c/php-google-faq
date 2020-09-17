05b576c6da553e50ac9e258968995c21c4a43138
Riccardo Colombo <rikyregna@gmail.com>
2020-09-17 15:53:47 +0200

    add style

--- index.php
+++ index.php
@@ -4,15 +4,76 @@ Ci sono diverse domande con relative risposte. Gestire il “Database” e la vi
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="stylesheet" href="css/style.css">
 	<title>Faq Google</title>
 </head>
+
 <body>
 
-<h1>prova</h1>
-	
+
+	<header>
+		<div class="logo">
+			<img src="https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_74x24dp.png" alt="">
+			<span class="policies">Privacy e termini</span>
+		</div>
+		<nav>
+			<ul>
+				<li><a href="">Introduzione</a></li>
+				<li><a href="">Norme sulla privacy</a></li>
+				<li><a href="">Termini di servizio</a></li>
+				<li><a href="">Tecnologie</a></li>
+				<li><a href="">Domande frequenti</a></li>
+			</ul>
+			<span class="my-account">tonystank@gmail.com</span>
+		</nav>
+
+
+	</header>
+
+	<main>
+		<div class="container">
+
+
+			<h2>TEsto testo testo</h2>
+			<div class="body">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corrupti sequi vero necessitatibus
+				distinctio voluptas suscipit quaerat ducimus odio temporibus earum dolorum quibusdam laborum minus, dicta
+				doloribus. Qui ipsa, tempora quisquam ea amet esse harum, sed dolorem magnam nam est. Deserunt soluta
+				cupiditate modi cumque possimus et aperiam, eveniet nemo voluptatibus sunt suscipit eaque nostrum non alias
+				blanditiis autem ipsam! Esse, illo? Eius repellendus molestias, optio dolorem saepe doloremque deserunt
+				perferendis, adipisci aspernatur laborum assumenda quia. Et provident doloribus, consequuntur nisi eius
+				mollitia eaque vel dolorem harum, alias sequi neque a laudantium quisquam obcaecati quam reiciendis commodi
+				sapiente nesciunt. Neque commodi magni, aperiam harum totam fugit voluptatem reiciendis nulla et sed vitae
+				iure accusamus, quis quo nemo officiis explicabo perferendis temporibus suscipit architecto error, similique
+				omnis qui voluptates. Explicabo iusto et doloremque veritatis? Nemo, labore facere obcaecati odio veniam
+				voluptate saepe a ut recusandae dolorum provident vitae ipsum, repellat quas voluptatum eveniet doloribus?
+				Sapiente perspiciatis eaque suscipit in itaque, obcaecati accusantium architecto, aliquam id voluptates
+				exercitationem ipsa amet pariatur illo et ea? Veritatis assumenda perspiciatis dolorum, tempore quis sed
+				maiores atque ipsum beatae aliquid minus, sit voluptatum architecto temporibus perferendis nulla deleniti
+				amet sequi placeat sint maxime repudiandae! Vitae, delectus cupiditate.</div>
+
+
+
+
+		</div>
+
+
+
+
+	</main>
+
+
+	<footer>
+
+	</footer>
+
+
+
+
+
 </body>
+
 </html>
